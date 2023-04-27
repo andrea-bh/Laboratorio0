456b1689066e576daceadc1f3407efa328c1ebeb
Andrea <andreabejaraano@gmail.com>
2023-04-26 18:15:06 -0600

    Ejercicio 05

--- Laboratorio04/Ejercicio05.cs
+++ Laboratorio04/Ejercicio05.cs
@@ -2,15 +2,40 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Laboratorio04
 {
     public class Ejercicio05
     {
-        public int[] CalcularBilletes(int cantidad, int[,] billetes)
+        private int monto;
+
+        public int[] CalcularBilletes(int monto, int[,] billetes)
         {
-            return new int[] {};
+            int[] total = new int[billetes.GetLength(0)];
+
+            for (int i = 0; i < total.Length; i++)
+            {
+                int billetesdeno = billetes[i, 0];
+                int numerobillete = billetes[i, 1];
+
+                int billetealeatorio = monto / billetesdeno;
+
+                if (numerobillete < 0 || billetealeatorio <= numerobillete)
+                {
+                    total[i] = billetealeatorio;
+                }
+                else
+                {
+                    total[i] = numerobillete;
+                }
+                monto = monto - (total[i] * billetesdeno);
+            }
+
+
+            Console.ReadKey();
+            return total;
         }
     }
 }
